c84781951bf212534297de79dc7b26a202a686ea
M-Saif-ARG <msaghori2003@gmail.com>
2024-07-25 20:42:10 +0530

    second

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,28 +1,32 @@
-import React, {useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import bg1 from '../images/bg1.png';
 import bg2 from '../images/bg2.png';
 import bg3 from '../images/bg3.png';
+import chat from '../images/chat.png';
 import { NavLink, useNavigate } from 'react-router-dom';
+
 const Home = () => {
-
     const navigate = useNavigate();
+    const [showChatWindow, setShowChatWindow] = useState(false);
 
-    useEffect(() =>{
-        if(!localStorage.getItem("token"))
+    useEffect(() => {
+        if (!localStorage.getItem("token")) {
             navigate("/signup");
+        }
     }, []);
+
+    const toggleChatWindow = () => {
+        setShowChatWindow(!showChatWindow);
+    };    
 
     return (
         <>
-            {/* <!-- Main header --> */}
-
+            {/* Main header */}
             <div className="container-fluid main_header">
                 <div className="row">
                     <div className="col-md-10 col-12 mx-auto">
                         <div className="row">
-
-                            {/* <!-- left side div --> */}
-
+                            {/* Left side div */}
                             <div className="col-md-6 col-12 main_header_left">
                                 <NavLink to="/suggest"> <button>Suggest A Car</button></NavLink>
                                 <br /><br />
@@ -30,9 +34,7 @@ const Home = () => {
                                 <br /><br />
                                 <NavLink to="/rent"> <button>Rent A Car</button></NavLink>
                             </div>
-
-                            {/* <!-- right side div --> */}
-
+                            {/* Right side div */}
                             <div className="col-md-6 col-12 main_header_right">
                                 <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
                                     <div className="carousel-inner">
@@ -60,8 +62,23 @@ const Home = () => {
                     </div>
                 </div>
             </div>
+            
+            {/* Chat window */}
+            {showChatWindow && (
+                <div className="chat-window">
+                    <iframe
+                        src="https://cdn.botpress.cloud/webchat/v2/shareable.html?botId=cbed452d-aff1-4944-9f07-280a82a3aa21"
+                        title="Chat Window"
+                        width="100%"
+                        height="100%"
+                    />
+                </div>
+            )}
 
-
+            {/* Chat icon */}
+            <div className="chat-icon-container" onClick={toggleChatWindow}>
+                <img src={chat} alt="Chat" className="chat-icon" />
+            </div>
         </>
     );
 };
